3621d08ecbcc0aac6cb0a6088d567fac5459c0b1
psycho00730 <cruzjared0930@gmail.com>
2023-03-13 21:45:16 +0800

    re commit

--- app/src/main/java/com/example/newcopy/MainActivity.java
+++ app/src/main/java/com/example/newcopy/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.myapplication.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
